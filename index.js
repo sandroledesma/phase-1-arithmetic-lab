b40290de02c7417e3ab235a648c6b43cf76b3e44
Alessandro <sandroledesma@gmail.com>
2024-04-27 17:24:41 -0400

    add files

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1 * num2;
+
+
+const random = Math.ceil(Math.random() * 100);
+
+let num3 = 16;
+let num4 = 4;
+
+const mod = num3 / num4;
+
+let max;
+
+max = Math.max(3, 7, 12, 17, 20);
+
